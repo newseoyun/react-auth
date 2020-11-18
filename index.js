52a8919dd5c5d290c2d6ebce219b3382759dcb73
seoyun <newseoyun@gmail.com>
2020-11-18 23:15:47 +0900

    login까지

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const config = require('./config/key');
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { User } = require('./models/User');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
@@ -24,12 +26,51 @@ app.post('/register', (req, res) => {
 
     user.save((err, doc) => {
         if (err) return res.json({success: false, err });
+
         return res.status(200).json({
             success: true
         });
     });
 });
 
+app.post('/login', (req, res) => {
+    // 유저 검증
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "회원 정보가 없습니다."
+            })
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "비밀번호 틀림"
+                })
+            }
+
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true,
+                        userId: user._id
+                    })
+            })
+        })
+
+    })
+
+
+    // 있으면 토큰 생성
+
+
+
+})
 
 
 app.listen(port, () => console.log(`server run port ${port}`));
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; /* salt 글자수 */
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -31,6 +34,56 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        //비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err)
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+
+})
+
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if (err) return callback(err);
+
+        callback(null, isMatch);
+
+    })
+}
+
+// jwt를 이용하여 토큰 생성
+userSchema.methods.generateToken = function(callback) {
+
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token;
+    user.save(function(err, user) {
+        if (err) return callback(err)
+
+        callback(null, user)
+    })
+
+}
+
+
+
+
 
 const User = mongoose.model('User', userSchema);
 
